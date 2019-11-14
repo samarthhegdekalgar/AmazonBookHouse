6299a19af517d6f000f6abd58b5e0860c527d0ee
Akashdeep <akashdp9@gmail.com>
2019-11-14 14:50:20 +0530

    email unique

--- src/User/models.py
+++ src/User/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
     password = models.CharField(max_length=32)
     is_deleted = models.BooleanField(default=False)
 
