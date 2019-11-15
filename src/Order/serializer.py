e1e7142d9f5c24b080915e5076ac33d3d153da5c
akashdp9 <53440881+akashdp9@users.noreply.github.com>
2019-11-15 14:35:16 +0530

    Orderapp (#12)
    * setup
    
    * models,serializers and admin
    
    * views and urls
    
    * email unique
    
    * email unique
    
    * Models has been created for book app and registered in admin page
    
    * creation of serializer and view is done, create,update,delete and search functionalities are working as per requirement
    
    * order models has been created and admin registration is done
    
    * new function calculate total price has been added
    
    * test message
    
    * merging with master
    
    * views and serializer

---
+++ src/Order/serializer.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Order
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    user_name = serializers.ReadOnlyField(source='user.name')
+    book_name = serializers.ReadOnlyField(source='book.name')
+
+    class Meta:
+        model = Order
+        fields = ('user_name', 'book_name', 'total_price', 'is_cancelled')
+
+
+class OrderUpdateSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.name')
+    book_name = serializers.CharField(source='book.name')
+
+    class Meta:
+        model = Order
+        fields = ('pk', 'user_name', 'book_name', 'total_price', 'is_cancelled')
--- src/Order/views.py
+++ src/Order/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from .serializer import OrderSerializer, OrderUpdateSerializer
+from .models import Order
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView
 
-# Create your views here.
+
+class OrderListAPIView(ListAPIView):
+    queryset = Order.objects.all().filter(is_cancelled=False)
+    serializer_class = OrderSerializer
+
+
+class OrderCreateAPIView(CreateAPIView):
+    serializer_class = OrderSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class OrderUpdateAPIView(UpdateAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderUpdateSerializer
+    lookup_field = 'pk'
+
+
+class OrderDestroyAPIView(DestroyAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderUpdateSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        instance.is_cancelled = True
+        instance.save()
+
