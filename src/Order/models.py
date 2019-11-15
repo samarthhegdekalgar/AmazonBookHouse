ae9d277ae5495e3ecfbd4220ee87ad9bf76aa96e
Akashdeep <akashdp9@gmail.com>
2019-11-15 12:14:55 +0530

    new function calculate total price has been added

--- src/Order/models.py
+++ src/Order/models.py
@@ -5,8 +5,12 @@ from django.db import models
 class Order(models.Model):
     user = models.ForeignKey('User.User', on_delete=models.CASCADE)
     book = models.ForeignKey('Book.Book', on_delete=models.CASCADE)
-    total_price = models.IntegerField()
-    is_cancelled = models.BooleanField()
+    total_price = models.IntegerField(default=0, null=True)
+    is_cancelled = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return str(self.user)
+
+    def total_price_calculate(self):
+        self.total_price += self.book.price
+        return self.total_price
